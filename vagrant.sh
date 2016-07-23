e49457440ce8dc701f55e9a2f31072812d4f6f8d
Leonard Shtika <leoshtika@gmail.com>
2016-07-23 14:57:38 +0300

    Add MySQL installation

--- vagrant.sh
+++ vagrant.sh
@@ -3,9 +3,6 @@
 # Update the list of available packages
 apt-get -y update
 
-# Install GIT
-# apt-get install -y git
-
 # Installing Apache
 apt-get install -y apache2
 
@@ -16,29 +13,28 @@ if ! [ -L /var/www/html ]; then
 fi
 
 # Change AllowOverride in apache2.conf for the .htaccess to work
-# sed -i '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
+sed -i '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
+
 # Enable Apache's mod_rewrite
-# sudo a2enmod rewrite
+sudo a2enmod rewrite
 
 # Installing MySQL and it's dependencies, Also, setting up root password for MySQL as it will prompt to enter the password during installation
-# debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password pass123'
-# debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password pass123'
-# apt-get -y install mysql-server libapache2-mod-auth-mysql
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password pass123'
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password pass123'
+apt-get -y install mysql-server libapache2-mod-auth-mysql
 
 # Installing PHP and it's dependencies
-apt-get -y install php5 libapache2-mod-php5 php5-mcrypt curl php5-curl php5-mysql
-# apt-get -y install php5-imagick
-# apt-get -y install php5-intl
-
-# Install Composer
-# if [ ! -f /usr/local/bin/composer ]; then
-#     curl -sS https://getcomposer.org/installer | php
-#     mv composer.phar /usr/local/bin/composer
-# fi
-
-# Install 'composer-asset-plugin'
-# echo "Installing Composer Asset Plugin"
-# composer global require "fxp/composer-asset-plugin:~1.1.1"
+apt-get -y install php5 libapache2-mod-php5 php5-mcrypt curl php5-curl php5-mysql php5-intl
 
 # Restart Apache
 service apache2 restart
+
+# Install Composer
+if [ ! -f /usr/local/bin/composer ]; then
+    curl -sS https://getcomposer.org/installer | php
+    mv composer.phar /usr/local/bin/composer
+fi
+
+echo "================================"
+echo "Your LAMP stack is ready for use"
+echo "================================"
